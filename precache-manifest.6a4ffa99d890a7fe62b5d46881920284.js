9f1bbd04a286be36b0d088287fbe1ad4415b2d9e
itPointOfLife <it_original@ukr.net>
2020-05-14 13:19:33 +0300

    Updates

---
+++ precache-manifest.6a4ffa99d890a7fe62b5d46881920284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee3199fe6f5203449c9e388a958d164",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "ef94d1020bf6308617fc",
+    "url": "/todo/static/css/main.ac8b736b.chunk.css"
+  },
+  {
+    "revision": "c7a8aaa2a061e0f61c9d",
+    "url": "/todo/static/js/2.44ce746c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.44ce746c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef94d1020bf6308617fc",
+    "url": "/todo/static/js/main.5ab0f857.chunk.js"
+  },
+  {
+    "revision": "67114fc4ad6958319bc3",
+    "url": "/todo/static/js/runtime-main.e29500e7.js"
+  }
+]);
