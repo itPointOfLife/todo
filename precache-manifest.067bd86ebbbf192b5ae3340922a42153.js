25613a0bec35ad560ed162ca1c2003bdaaa233b4
itPointOfLife <it_original@ukr.net>
2020-05-14 13:39:40 +0300

    Updates

--- precache-manifest.067bd86ebbbf192b5ae3340922a42153.js
+++ precache-manifest.067bd86ebbbf192b5ae3340922a42153.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aee3199fe6f5203449c9e388a958d164",
+    "revision": "69e2d6bc8e961511a1fd0e2fac5eaa6f",
     "url": "/todo/index.html"
   },
   {
-    "revision": "ef94d1020bf6308617fc",
-    "url": "/todo/static/css/main.ac8b736b.chunk.css"
+    "revision": "a9155b7cb9546ed54da9",
+    "url": "/todo/static/css/main.d7c442ae.chunk.css"
   },
   {
     "revision": "c7a8aaa2a061e0f61c9d",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/todo/static/js/2.44ce746c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ef94d1020bf6308617fc",
+    "revision": "a9155b7cb9546ed54da9",
     "url": "/todo/static/js/main.5ab0f857.chunk.js"
   },
   {
